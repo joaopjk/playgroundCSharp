c66b989d6ba1b654ed6cf66d00b24ef5ceffdbc2
joaopjk <joaopjk48@gmail.com>
2024-10-31 14:11:45 -0300

    Implement of AggregateRoot

---
+++ CQRS.Core/Domain/AggregateRoot.cs
@@ -0,0 +1,38 @@
+﻿using CQRS.Core.Events;
+
+namespace CQRS.Core.Domain;
+
+public class AggregateRoot
+{
+    protected Guid _id;
+    public Guid Id => _id;
+    private readonly List<BaseEvent> _uncommittedEvents = new();
+    public int Version { get; set; } = -1;
+
+    public IEnumerable<BaseEvent> GetUncommittedEvents() => _uncommittedEvents;
+    public void MarkChangesAsCommitted() => _uncommittedEvents.Clear();
+
+    private void ApplyChanges(BaseEvent @event, bool isNew)
+    {
+        var method = this.GetType().GetMethod("Apply", new Type[] { @event.GetType() });
+
+        if (method == null)
+            throw new ArgumentNullException($"Method {@event.GetType().Name} not found!");
+
+        method.Invoke(this, new object[] { @event });
+
+        if (isNew)
+            _uncommittedEvents.Add(@event);
+    }
+
+    protected void RaiseEvent(BaseEvent @event)
+    {
+        ApplyChanges(@event, true);
+    }
+
+    public void ReplayEvents(IEnumerable<BaseEvent> events)
+    {
+        foreach (var @event in events)
+            ApplyChanges(@event, false);
+    }
+}
---
+++ Post.Cmd.Infrastructure/Dispatchers/CommandDispatcher.cs
@@ -0,0 +1,27 @@
+﻿using CQRS.Core.Commands;
+using CQRS.Core.Infrastructure;
+
+namespace Post.Cmd.Infrastructure.Dispatchers;
+
+public class CommandDispatcher : ICommandDispatcher
+{
+    private readonly Dictionary<Type, Func<BaseCommand, Task>> _handlers = new();
+
+    public void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand
+    {
+        if (_handlers.ContainsKey(typeof(T)))
+            throw new IndexOutOfRangeException($"Handler {typeof(T).FullName} has already been registered");
+
+        _handlers.Add(typeof(T), h => handler((T)h));
+    }
+
+    public async Task SendAsync(BaseCommand command)
+    {
+        if (_handlers.TryGetValue(command.GetType(), out var handler))
+            await handler(command);
+        else
+        {
+            throw new ArgumentNullException($"Handler {command.GetType().FullName} is not registered");
+        }
+    }
+}
