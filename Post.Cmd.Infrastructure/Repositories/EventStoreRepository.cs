0bd48f730bc8513e8c80f8971f13c1098817f590
joaopjk <joaopjk48@gmail.com>
2024-11-01 23:55:12 -0300

    Implement of EventStoreRepository

---
+++ Post.Cmd.Infrastructure/Repositories/EventStoreRepository.cs
@@ -0,0 +1,32 @@
+﻿using CQRS.Core.Events;
+using CQRS.Core.Repositories;
+using MongoDB.Driver;
+
+namespace Post.Cmd.Infrastructure.Repositories;
+
+public class EventStoreRepository : IEventStoreRepository
+{
+    private readonly IMongoCollection<EventModel> _collection;
+
+    public EventStoreRepository()
+    {
+        var mongoClient = new MongoClient(Environment.GetEnvironmentVariable("CONNECTION_STRING"));
+        var database = mongoClient.GetDatabase(Environment.GetEnvironmentVariable("MONGO_DATABASE"));
+
+        _collection = database.GetCollection<EventModel>(Environment.GetEnvironmentVariable("COLLECTION"));
+    }
+
+    public async Task SaveAsync(EventModel @event)
+    {
+        await _collection.InsertOneAsync(@event)
+            .ConfigureAwait(false);
+    }
+
+    public async Task<List<EventModel>> FindByAggregateId(Guid aggregateId)
+    {
+        return await _collection.Find(c =>
+                c.AggregateIdentifier == aggregateId)
+            .ToListAsync()
+            .ConfigureAwait(false);
+    }
+}
