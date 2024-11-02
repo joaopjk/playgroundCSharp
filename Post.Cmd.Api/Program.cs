71f7d41565c383ac47f4e3b25d62f6e968da3220
joaopjk <joaopjk48@gmail.com>
2024-11-02 00:52:08 -0300

    Add command handler dispatchers

--- Post.Cmd.Api/Program.cs
+++ Post.Cmd.Api/Program.cs
@@ -3,6 +3,7 @@ using CQRS.Core.Infrastructure;
 using CQRS.Core.Repositories;
 using Post.Cmd.Api.Commands;
 using Post.Cmd.Domain.Aggregates;
+using Post.Cmd.Infrastructure.Dispatchers;
 using Post.Cmd.Infrastructure.Handlers;
 using Post.Cmd.Infrastructure.Repositories;
 using Post.Cmd.Infrastructure.Stores;
@@ -12,7 +13,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 builder.Services.AddScoped<IEventStore, EventStore>();
 builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
-builder.Services.AddScoped<ICommandHandler,CommandHandler>();
+builder.Services.AddScoped<ICommandHandler, CommandHandler>();
+
+// Register command handler methods
+#pragma warning disable ASP0000
+var commandHandler = builder.Services?.BuildServiceProvider().GetRequiredService<ICommandHandler>();
+#pragma warning restore ASP0000
+
+var dispatcher = new CommandDispatcher();
+dispatcher.RegisterHandler<NewPostCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<EditMessageCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<LikePostCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<AddCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<EditCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<RemoveCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<DeletePostCommand>(commandHandler.HandleAsync);
+builder.Services.AddSingleton<ICommandDispatcher>(_ => dispatcher);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
