1f9376409e6a413c15854160d59666940593c5fc
joaopjk <joaopjk48@gmail.com>
2024-11-01 23:43:42 -0300

    Create of IEventStoreRepository

---
+++ CQRS.Core/Repositories/IEventStoreRepository.cs
@@ -0,0 +1,9 @@
+﻿using CQRS.Core.Events;
+
+namespace CQRS.Core.Repositories;
+
+public interface IEventStoreRepository
+{
+    Task SaveAsync(EventModel @event);
+    Task<List<EventModel>> FindByAggregateId(Guid aggregateId);
+}
